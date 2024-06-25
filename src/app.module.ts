4b5cb08078b24538e3f911b943525bd3ead3f548
jonassborges <jonas.borges182@gmail.com>
2024-06-25 17:14:37 -0300

    fix: fixed mongodb url connection to use .env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://jowb:Risoto%40182@cluster0.hh8e9an.mongodb.net/smart_ranking?retryWrites=true&w=majority&appName=Cluster0',
-    ),
-    PlayersModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL), PlayersModule],
   controllers: [],
   providers: [],
 })
